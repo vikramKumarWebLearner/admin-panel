2b8f16889cad14a97bcf08ba74b4cf0673f7a2d3
vikram kumar <vikrampchoudhary777@gmail.com>
2025-01-11 10:41:25 +0530

    code remove

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\BranController;
-use App\Http\Controllers\Api\CategoryController;
-use App\Http\Controllers\Api\ProductController;
-use App\Http\Controllers\Api\SubCategoryController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -21,7 +18,3 @@ use App\Http\Controllers\Api\SubCategoryController;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
 	return $request->user();
 });
-Route::get('/categories', [CategoryController::class,'index']);
-Route::get('/sub-categries', [SubCategoryController::class,'index']);
-Route::get('/brands', [BranController::class,'index']);
-Route::get('/products', [ProductController::class,'index']);
